f289eee5c96a472c8d07340eceed0512dc2c767f
Falcon <endless640c@gmail.com>
2025-05-27 10:18:49 +0800

    feat: 移除 console log

--- pages/api/update-metafield.ts
+++ pages/api/update-metafield.ts
@@ -43,7 +43,6 @@ export default async function handler(
     (metafield: { namespace: string; key: string; id: string }) => metafield.namespace === namespace && metafield.key === key
   );
   const metafieldId = existingMetafield?.id;
-  console.log(metafieldId);
 
   // 如果有 metafieldId，則改為更新
   if (metafieldId) {
